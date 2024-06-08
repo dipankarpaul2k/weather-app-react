f72deb3cc3fa6b9ed0b8b79911f8beeeb18f25e9
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-06-08 12:25:03 +0530

    add country country feature codes

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import useWeatherContext from "../contexts/useWeatherContext";
 import { placeFeatureCodes } from "../utils/constants";
@@ -29,40 +30,58 @@ export default function MapComponent() {
   }
 
   return (
-    <div className="p-4">
-      <div className="grid grid-cols-1 gap-4">
+    <div className="py-4">
+      <h3 className="text-xl font-bold mb-4">City Information</h3>
+      <div className="grid grid-cols-1 gap-y-4">
         {/* location info */}
         <div className="">
-          <div className="stats stats-horizontal shadow w-full">
+          <div className="stats stats-horizontal shadow w-full border">
+            {/* city */}
             <div className="stat">
               <div className="stat-title">City</div>
               <div className="stat-value">
-                <span>{cityDetails.name}</span>
-                <span>, </span>
-                <span
-                  className="lg:tooltip font-medium"
-                  data-tip={featureCodeMeaning}
-                >
-                  <span className="text-sm">{cityDetails.feature_code}</span>
+                <span>{cityDetails.name}, </span>
+                <span className="text-sm font-medium">
+                  {cityDetails.feature_code}
                 </span>
               </div>
-              <div className="max-lg:stat-desc">{featureCodeMeaning}</div>
+              <div className="stat-desc">
+                <span>
+                  {cityDetails.feature_code}: {featureCodeMeaning}
+                </span>
+              </div>
             </div>
 
-            <div className="stat">
-              <div className="stat-title">State</div>
-              <div className="stat-value">{cityDetails.admin1}</div>
-            </div>
+            {/* state */}
+            {cityDetails.admin1 && (
+              <div className="stat">
+                <div className="stat-title">State</div>
+                <div className="stat-value">{cityDetails.admin1}</div>
+              </div>
+            )}
 
+            {/* country */}
             <div className="stat">
               <div className="stat-title">Country</div>
               <div className="stat-value">
-                <span>{cityDetails.country}</span>
-                <span>, </span>
-                <span className="text-sm font-medium">
-                  {cityDetails.country_code}
-                </span>
+                <span>{cityDetails.country}, </span>
+                <div className="avatar">
+                  <div className="w-4 rounded-full">
+                    <img
+                      src={`https://hatscripts.github.io/circle-flags/flags/${cityDetails.country_code.toLowerCase()}.svg`}
+                      alt="Country Flag Image"
+                    />
+                  </div>
+                </div>
               </div>
+              <div className="stat-desc">
+                <span>Country Code: {cityDetails.country_code}</span>
+              </div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-title">Time Zone</div>
+              <div className="stat-value">{cityDetails.timezone}</div>
             </div>
 
             <div className="stat">
@@ -74,56 +93,59 @@ export default function MapComponent() {
               <div className="stat-title">Longitude</div>
               <div className="stat-value">{cityDetails.longitude}</div>
             </div>
-
-            <div className="stat">
-              <div className="stat-title">Time Zone</div>
-              <div className="stat-value">{cityDetails.timezone}</div>
-            </div>
           </div>
         </div>
 
-        {/* map grid */}
-        <div
-          id="map"
-          className="col-span-2 min-h-[500px] w-full rounded-xl overflow-hidden"
-        >
+        {/* map */}
+        <div className="w-full min-h-[400px] px-4 sm:px-0">
           {lat && lon && (
-            <MapContainer
-              className="h-full w-full"
-              center={[lat, lon]}
-              zoom={10}
-              scrollWheelZoom={true} // Enable scroll wheel zoom
-              touchZoom={false} // Disable touch zooming
-              dragging={false} // Disable map dragging
-            >
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              />
-              <Marker position={[lat, lon]}>
-                <Popup>
-                  <div>
-                    <p>
-                      <strong>Location: </strong>
-                      {cityDetails.name}, {cityDetails.admin1},{" "}
-                      {cityDetails.country}
-                    </p>
-                    <p>
-                      <strong>Time Zone: </strong>
-                      {cityDetails.timezone}
-                    </p>
-                    <p>
-                      <strong>Latitude: </strong>
-                      {cityDetails.latitude}
-                    </p>
-                    <p>
-                      <strong>Longitude: </strong>
-                      {cityDetails.longitude}
-                    </p>
-                  </div>
-                </Popup>
-              </Marker>
-            </MapContainer>
+            <div className="h-full w-full rounded-xl overflow-hidden">
+              <MapContainer
+                className="h-full w-full"
+                center={[lat, lon]}
+                zoom={10}
+                scrollWheelZoom={true} // Enable scroll wheel zoom
+                touchZoom={false} // Disable touch zooming
+                dragging={false} // Disable map dragging
+              >
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[lat, lon]}>
+                  <Popup>
+                    <div>
+                      <p>
+                        <strong>City: </strong>
+                        {cityDetails.name}
+                      </p>
+                      {cityDetails.admin1 && (
+                        <p>
+                          <strong>State: </strong>
+                          {cityDetails.admin1}
+                        </p>
+                      )}
+                      <p>
+                        <strong>Country: </strong>
+                        {cityDetails.country}
+                      </p>
+                      <p>
+                        <strong>Time Zone: </strong>
+                        {cityDetails.timezone}
+                      </p>
+                      <p>
+                        <strong>Latitude: </strong>
+                        {cityDetails.latitude}
+                      </p>
+                      <p>
+                        <strong>Longitude: </strong>
+                        {cityDetails.longitude}
+                      </p>
+                    </div>
+                  </Popup>
+                </Marker>
+              </MapContainer>
+            </div>
           )}
         </div>
       </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -39,90 +39,171 @@ export const themes = [
 ];
 
 export const placeFeatureCodes = [
+  // Administrative Boundary Features codes
   {
-    id: randomId(),
+    code: "ADM1",
+    meaning: "First-order administrative division (e.g., a state or province)",
+  },
+  {
+    code: "ADM2",
+    meaning: "Second-order administrative division (e.g., a county)",
+  },
+  {
+    code: "ADM3",
+    meaning: "Third-order administrative division (e.g., a district)",
+  },
+  {
+    code: "ADM4",
+    meaning: "Fourth-order administrative division",
+  },
+  {
+    code: "ADM5",
+    meaning: "Fifth-order administrative division",
+  },
+  {
+    code: "ADM1H",
+    meaning: "Historical first-order administrative division",
+  },
+  {
+    code: "ADM2H",
+    meaning: "Historical second-order administrative division",
+  },
+  {
+    code: "ADM3H",
+    meaning: "Historical third-order administrative division",
+  },
+  {
+    code: "ADM4H",
+    meaning: "Historical fourth-order administrative division",
+  },
+  {
+    code: "ADM5H",
+    meaning: "Historical fifth-order administrative division",
+  },
+  {
+    code: "ADMD",
+    meaning: "Administrative division",
+  },
+  {
+    code: "ADMF",
+    meaning: "Administrative facility",
+  },
+  {
+    code: "PCL",
+    meaning: "Political entity (e.g., a country)",
+  },
+  {
+    code: "PCLD",
+    meaning: "Dependent political entity",
+  },
+  {
+    code: "PCLF",
+    meaning: "Freely associated state",
+  },
+  {
+    code: "PCLH",
+    meaning: "Historical political entity",
+  },
+  {
+    code: "PCLI",
+    meaning: "Independent political entity",
+  },
+  {
+    code: "PCLIX",
+    meaning: "Section of independent political entity",
+  },
+  {
+    code: "PCLS",
+    meaning: "Semi-independent political entity",
+  },
+  {
+    code: "LTER",
+    meaning: "Leased area",
+  },
+  {
+    code: "PRSH",
+    meaning: "Parish",
+  },
+  {
+    code: "TERR",
+    meaning: "Territory",
+  },
+  {
+    code: "ZN",
+    meaning: "Zone",
+  },
+  {
+    code: "ZNB",
+    meaning: "Buffer zone",
+  },
+  // Populated Place Features codes
+  {
     code: "PPL",
     meaning: "Populated place",
   },
   {
-    id: randomId(),
     code: "PPLA",
     meaning:
       "Seat of a first-order administrative division (e.g., a state capital)",
   },
   {
-    id: randomId(),
     code: "PPLA2",
     meaning:
       "Seat of a second-order administrative division (e.g., a county seat)",
   },
   {
-    id: randomId(),
     code: "PPLA3",
     meaning: "Seat of a third-order administrative division",
   },
   {
-    id: randomId(),
     code: "PPLA4",
     meaning: "Seat of a fourth-order administrative division",
   },
   {
-    id: randomId(),
     code: "PPLC",
     meaning: "Capital of a political entity (e.g., a country capital)",
   },
   {
-    id: randomId(),
     code: "PPLCH",
     meaning: "Historical capital of a political entity",
   },
   {
-    id: randomId(),
     code: "PPLF",
     meaning: "Farm village",
   },
   {
-    id: randomId(),
     code: "PPLG",
     meaning: "Seat of government of a political entity",
   },
   {
-    id: randomId(),
     code: "PPLH",
     meaning: "Historical populated place that no longer exists",
   },
   {
-    id: randomId(),
     code: "PPLL",
     meaning: "Populated locality",
   },
   {
-    id: randomId(),
     code: "PPLQ",
     meaning: "Abandoned populated place",
   },
   {
-    id: randomId(),
     code: "PPLR",
     meaning: "Religious populated place",
   },
   {
-    id: randomId(),
     code: "PPLS",
     meaning: "Populated places",
   },
   {
-    id: randomId(),
     code: "PPLW",
     meaning: "Destroyed populated place",
   },
   {
-    id: randomId(),
     code: "PPLX",
     meaning: "Section of populated place",
   },
   {
-    id: randomId(),
     code: "STLMT",
     meaning: "Israeli settlement",
   },
